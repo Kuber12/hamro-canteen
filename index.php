5c8cde9dc54fa3e2a64362a1ccfde0dc4d34c803
kuber12 <kubershakya123@hotmail.com>
2023-04-10 22:45:58 +0545

    created layout for userprofile and cart

--- index.php
+++ index.php
@@ -7,26 +7,56 @@
                 <img src="./assets/logo.png "class= "hero-logo">
             </span>
             <span>
-                <div class="click-menu">
-                    <i class="fa-solid fa-cart-shopping hero-cart"></i>
-                    <div class="cart-menu hero-menu">
-                        <ul>
-                        <li><img src="./assets/logo.png" alt="user-profile"></li>
-                        <li><a href="#">Menu item 2</a></li>
-                        <li><a href="#">Menu item 3</a></li>
-                        </ul>
+                <i class="fa-solid fa-cart-shopping" id="hero-cart-button"></i>
+                <div id="myPopup" class="popup">
+                    <div class="cart-menu-pop popup-content" >
+                        <h1>My Orders</h1>
+                        <div class="cart-menu-items">
+                            <img class="item-image" src="assets/item.png" alt="item">
+                            <h3 class="item-name">Momo</h3>
+                            <h3 class="item-price">Rs 120</h3>
+                            <input type="number" name="quantity" class="quantity">
+                            <h3 class="item-equalto">=</h3>
+                            <h3 class="item-total">240</h3>
+                            <h3 class="item-delete">x</h3>
+                        </div>
+                        <div class="cart-menu-items">
+                            <img class="item-image" src="assets/item.png" alt="item">
+                            <h3 class="item-name">Momo</h3>
+                            <h3 class="item-price">Rs 120</h3>
+                            <input type="number" name="quantity" class="quantity">
+                            <h3 class="item-equalto">=</h3>
+                            <h3 class="item-total">240</h3>
+                            <h3 class="item-delete">x</h3>
+                        </div>
+                        <div class="cart-menu-items">
+                            <img class="item-image" src="assets/item.png" alt="item">
+                            <h3 class="item-name">Momo</h3>
+                            <h3 class="item-price">Rs 120</h3>
+                            <input type="number" name="quantity" class="quantity">
+                            <h3 class="item-equalto">=</h3>
+                            <h3 class="item-total">240</h3>
+                            <h3 class="item-delete">x</h3>
+                        </div>
+                        <h2 style="text-align:right">Total = <span class="items-total">240
+                        </span></h2>
                     </div>
                 </div>
-                <div class="click-menu">
-                    <i class="fa-solid fa-user hero-profile"></i>
-                    <div class="profile-menu hero-menu">
-                        <ul>
-                        <li><a href="#">Menu item 1</a></li>
-                        <li><a href="#">Menu item 2</a></li>
-                        <li><a href="#">Menu item 3</a></li>
-                        </ul>
+                <div class="dropdown" >
+                    <i class="fa-solid fa-user dropbtn" id="hero-profile-button" ></i>
+                    <div class="dropdown-content" id="profile-menu">
+                        <img src="./assets/logo.png" alt="user-profile">
+                        <a>My Profile</a>
+                        <a>Change Password</a>
+                        <a>Help</a>
+                        <a id="sign-out">Sign Out</a>
                     </div>
                 </div>
+                <!-- <div class="hero-menu" id="profile-menu">
+                    My profile
+                    
+                </div> -->
+                
             </span>
         </nav>
         <h1 class="hero-heading middle-centered-div">Hamro Canteen</h1>
@@ -73,3 +103,4 @@
 <?php
     include './layout/foot.php';
 ?>
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,39 @@
-const clickMenu = document.querySelectorAll('.click-menu');
-clickMenu[0].addEventListener('click', function() {
-    clickMenu[0].classList.toggle('active');
-    clickMenu[1].classList.remove('active');
+// popup
+let popup = document.getElementById("myPopup");
+let cartButton = document.getElementById("hero-cart-button");
+let profileButton = document.getElementById("hero-profile-button");
+let profileMenu = document.querySelector("#profile-menu");
+cartButton.addEventListener("click",function(){
+    openPopup();
+})
+// Function to open the popup
+function openPopup() {
+    popup.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    popup.style.display = "none";
+}
+
+// Close the popup when the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+// Add an event listener to the document that listens for a click
+document.addEventListener("click", function(event) {
+  // Check if the clicked element is not the dropdown menu itself and is not a child of the dropdown menu
+  if (!event.target.matches(".dropbtn") && !profileMenu.contains(event.target)) {
+    // Remove the "show" class from the dropdown menu
+    profileMenu.classList.remove("show");
+  }
 });
-clickMenu[1].addEventListener('click', function() {
-    clickMenu[1].classList.toggle('active');
-    clickMenu[0].classList.remove('active');
+
+// Add an event listener to the dropdown button that toggles the "show" class on the dropdown menu
+document.querySelector(".dropbtn").addEventListener("click", function() {
+    profileMenu.classList.toggle("show");
 });
+
+
