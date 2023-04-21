4c8d564b019c640f7c9523cdd4cea5dc23fb38d0
Shiv0346 <shivkumarghimire25@gmail.com>
2023-04-21 10:34:38 +0545

    popup updated

--- index.php
+++ index.php
@@ -4,32 +4,36 @@ include './layout/head.php';
 <div class="hero middle-centered">
     <div class="hero-items">
         <span>
-            <img src="./assets/logo-yellow.png " class="hero-logo">
+            <img src="./assets/logo-white.png " class="hero-logo">
         </span>
         <span>
-            <div class="click-menu">
-                <i class="fa-solid fa-cart-shopping fa-beat hero-cart"></i>
-                <div class="cart-menu hero-menu">
+            <i class="fa-solid fa-cart-shopping fa-beat hero-cart"></i>
+            <i class="fa-solid fa-user hero-profile"></i>
+            <div class="cart-menu">
+                <span><i class="fa-solid fa-circle-xmark fa-2xl" style="color: #000000;"></i></span>
             
-                    <ul>
-                        <li><img src="./assets/logo.png" alt="user-profile"></li>
-                        <li><a href="#">Menu item 2</a></li>
-                        <li><a href="#">Menu item 3</a></li>
-                    </ul>
+                <div class="cart-header">
+                    <h2>My Cart</h2>
                 </div>
-            </div>
-            <div class="click-menu">
-                <i class="fa-solid fa-user hero-profile"></i>
-                <div class="profile-menu hero-menu">
-                    <ul>
-                        <li><a href="#">Menu item 1</a></li>
-                        <li><a href="#">Menu item 2</a></li>
-                        <li><a href="#">Menu item 3</a></li>
-                    </ul>
+                
                 </div>
+
+            <div class="profile-menu">
+             <div class="profile-info-container">
+             <img src="./assets/avatar.jpg" alt="Profile Picture">
+             <p>Ram Prasad Subedi<p>
+             
+             </div>
+                <a href="#"><i class="fa-solid fa-circle-user"></i> My Profile </a>
+                <a href="#"><i class="fa-solid fa-cart-shopping"></i> My cart  </a>
+                <a href="#"><i class="fa-solid fa-circle-question"></i> Help </a>
+                <a href="#"> <i class="fa-solid fa-arrow-right-from-bracket"></i>Sign Out </a>
+
             </div>
-        </span>
+           </span>
+
     </div>
+
     <h1 class="hero-heading middle-centered-div">Hamro Canteen</h1>
     <div class="marquee">
         <marquee behavior="alternate" direction="left" width="100%">
@@ -37,18 +41,20 @@ include './layout/head.php';
         </marquee>
     </div>
 </div>
-<div id="menu"> <h3>Today's Menu</h3> </div>
+<div id="menu">
+    <h3>Today's Menu</h3>
+</div>
 
 <div class="menu-display">
     <div class="product">
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -60,11 +66,11 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -76,26 +82,11 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity:<i class="fa-solid fa-square-minus fa-lg" style="color: #000000;"></i>
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
-
-
-        </div>
-
-        <img class="order-btn" src="./assets/addtocart.png" alt="Add To Cart">
-
-    </div><div class="product">
-        <img src="./assets/item.png" alt="Product Image" class="product-image">
-        <span class="item-name">Momo</span>
-        <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -107,11 +98,11 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -123,11 +114,11 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -139,26 +130,43 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
 
         <img class="order-btn" src="./assets/addtocart.png" alt="Add To Cart">
 
-    </div><div class="product">
+    </div>
+    <div class="product">
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
+
+
+        </div>
+
+        <img class="order-btn" src="./assets/addtocart.png" alt="Add To Cart">
+
+    </div>
+    <div class="product">
+        <img src="./assets/item.png" alt="Product Image" class="product-image">
+        <span class="item-name">Momo</span>
+        <span class="price">RS. 120</span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
@@ -170,19 +178,20 @@ include './layout/head.php';
         <img src="./assets/item.png" alt="Product Image" class="product-image">
         <span class="item-name">Momo</span>
         <span class="price">RS. 120</span>
-        <div class="quantity">             
-            <span>Quantity: <button >-</button> 
-            <input type="number" value="1">
-            <button>+</button> 
-        </span>
+        <div class="quantity">
+            <span>Quantity: <i class="fa-solid fa-square-minus fa-lg" style="color: #000000; margin-left:3px;"></i>
+                <input type="number" value="1">
+                <i class="fa-solid fa-square-plus fa-lg"></i>
+            </span>
 
 
         </div>
 
         <img class="order-btn" src="./assets/addtocart.png" alt="Add To Cart">
-
+    
     </div>
-    <div class="popup"></div>
-<?php
-include './layout/foot.php';
-?>
+
+
+    <?php
+    include './layout/foot.php';
+    ?>
