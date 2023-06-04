67fcf217614564dbbe5735ad1f0e92b4b0c14e82
kuber12 <kubershakya123@hotmail.com>
2023-06-04 09:35:51 +0545

    changed colors in dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,6 @@
 <?php
     include './layout/head.php';
     include './layout/admin-sidebar.php';
-    include './layout/admin-sidebar.php';
 ?>
 <!-- chart js -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -10,11 +9,9 @@
   <div class="dashboard-middle-container">
     <div class="dashboard-middle-container-top">
       <div class="hello-admin">
-        <h3>Hello {admin}</h3>
-        <p>how are you today?</p>
+        <p>Hello {admin}</p>
       </div>
       <div class="canteen-status status-active">
-        <h3>Canteen Status</h3>
         <p class="canteen-active-now-message">
           <i class="fa-solid fa-circle status-circle"></i>Active now
         </p>
@@ -22,17 +19,52 @@
     </div>
     <div class="today-menu-heading">
       <h2 class="todays-menu-text">Today's Menu</h2>
-      <a href="http:/">Update Menu</a>
+      <button id="update-item-btn" onclick="location.href = './menuitem.php'">Update Menu</button>
     </div>
     <div class="dashboard-grid-container"> 
       <!-- dynamically added here -->
     </div> 
   </div>
   <div class="dashboard-sidebar-right">
+    <h2>Today's Special</h2>
+
+    <!-- Temporary code -->
+    <div class="pie">
+      <span class="message">We are still working on it</span>
+    </div>
+    <!-- <canvas id="popular-piechart"></canvas> -->
     <h2>Popular Right now</h2>
-    <canvas id="popular-piechart"></canvas>
-    <h2>Popular Right now</h2>
+    <div class="pie">
+      <span class="message">We are still working on it</span>
+    </div>
   </div>
+    <style>
+      .pie {
+        height:300px;
+        background-image: url('./assets/piechart.jpeg');
+        background-size: contain;
+        background-repeat:no-repeat;
+        position: relative;
+      }
+
+      .message {
+        display: none;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 18px;
+        color: white;
+        background-color: rgba(0, 0, 0, 0.8);
+        padding: 10px 20px;
+      }
+
+      .pie:hover .message {
+        display: block;
+      }
+    </style>
+    
+
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.js"></script>
@@ -50,7 +82,7 @@
             <img class="dashboard-item-image" src="assets/itemimage/${response[i]["itemImg"]}" alt="item">
             <div class="item-name-n-price">
               <span>${response[i]["itemName"]}</span>
-              <span>${response[i]["itemPrice"]}</span>
+              <span>Rs ${response[i]["itemPrice"]}</span>
             </div>
           </div>`;
           $(".dashboard-grid-container").append(elem);
